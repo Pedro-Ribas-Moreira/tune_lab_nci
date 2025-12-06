a11bb3ee6ea4a82efad1cefe9853fc2a59e0d73f
Pedro Moreira <phribasmoreira@gmail.com>
2025-12-06 22:22:53 +0000

    fixed form validation

--- scripts/login.js
+++ scripts/login.js
@@ -7,3 +7,41 @@ Pedro Moreira
   Higher Diploma in Computing  
   Web Design Client-Side Scripting
 */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("loginForm");
+  const usernameInput = document.getElementById("usernameInput");
+  const passwordInput = document.getElementById("passwordInput");
+  const forgotPasswordLink = document.getElementById("forgotPasswordLink");
+
+  forgotPasswordLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("An email to reset your password has been sent.");
+  });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    usernameInput.classList.remove("is-invalid");
+    passwordInput.classList.remove("is-invalid");
+
+    let isValid = true;
+
+    if (usernameInput.value.trim() === "") {
+      usernameInput.classList.add("is-invalid");
+      isValid = false;
+    }
+
+    if (passwordInput.value.trim() === "") {
+      passwordInput.classList.add("is-invalid");
+
+      isValid = false;
+    }
+
+    if (isValid) {
+      alert("Login Successful! ");
+
+      document.location.href = "index.html";
+    }
+  });
+});
